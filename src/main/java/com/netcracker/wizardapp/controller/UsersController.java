ee153da642de7565b776e586996e52f187da5026
BrazenFox <marokhonovdmitry1998@gmail.com>
2021-05-05 20:50:06 +0300

    results creator

--- src/main/java/com/netcracker/wizardapp/controller/UsersController.java
+++ src/main/java/com/netcracker/wizardapp/controller/UsersController.java
@@ -1,28 +1,11 @@
 package com.netcracker.wizardapp.controller;
 
-import com.netcracker.wizardapp.domain.Role;
-import com.netcracker.wizardapp.domain.Roles;
 import com.netcracker.wizardapp.domain.User;
-import com.netcracker.wizardapp.domain.Wizard;
-import com.netcracker.wizardapp.exceptions.RoleNotFoundException;
-import com.netcracker.wizardapp.exceptions.UserNotFoundException;
 import com.netcracker.wizardapp.payload.request.RegistrationRequest;
-import com.netcracker.wizardapp.payload.response.MessageResponse;
-import com.netcracker.wizardapp.repository.RoleRepo;
-import com.netcracker.wizardapp.repository.UserRepo;
-import com.netcracker.wizardapp.repository.WizardRepo;
 import com.netcracker.wizardapp.service.UsersService;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 
 @CrossOrigin(origins = "*")
 @RestController
